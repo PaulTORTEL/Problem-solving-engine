912c9fbb5ea605680fd2c29dd5b6cad8b1b9b96d
Vincent <vincent.coursac@gmail.com>
2017-03-01 00:36:17 +0100

    Remise à 0 des conneries de certains

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,8 @@
-#include <stdlib.h>
-#include <iostream>
-#include <fstream>
-#include <sstream>
 
-#include "parser/tokens.hpp"
+#include <iostream>
+
 
 int main(int argc, char** argv) {
-	parser::Token::initTables();
-
-	if(argc < 2) {
-		std::cerr << "Il faut specifier le fichier a charger." << std::endl;
-		return EXIT_FAILURE;
-	}
-
-	std::string contents;
-	std::string filename = argv[1];
-
-	std::ifstream* file = new std::ifstream();
-	file->open(filename, std::ios::in);
-
-	parser::Tokenizer tokens(file);
-
-	parser::Token tok;
-	do {
-		tok = tokens.nextToken();
-		std::cout << tok << std::endl;
-		if(tok.type == parser::TokenType::EndOfStream)
-			break;
-	} while(!tok.isError());
 
 
 	return EXIT_SUCCESS;
