acfd912ccd458caa2f4efbee3e398ee66bec03bd
Paul_TORTEL <paul.tortel@efrei.net>
2017-04-12 14:14:29 +0200

    rien

--- src/Sum.cpp
+++ src/Sum.cpp
@@ -12,7 +12,7 @@ Sum::Sum(std::vector<VarCoeff> involvedVars, std::string op, std::string type, i
         _resultNumber = resultNumber;
     else if (_type == "var")
         _resultVar = resultVar;
-
+std::cout << involvedVars.size() << " -> " << op << " / " << type << " / " << resultNumber << " / " << resultVar << std::endl;
 }
 
 Sum::~Sum()
@@ -52,12 +52,8 @@ bool Sum::isValuesPossibleForSum(std::vector<VarValue> varsValues) {
     }
 
     int result = 0;
-    for (int value : values) { // x - y - z ?
-            std::cout << "la value qu'on va add : " << value << " et le result : " << result << std::endl;
-            system("pause");
+    for (int value : values) // x - y - z ?
             result += value;
-    }
-    std::cout << "\t SORTIE : res : " << result << " l'op : " << _operator << " et le result  num : " << _resultNumber << std::endl;
 
     if (_type == "number") { // X1 + X2 = NUMBER
         if (_operator == "=") {
