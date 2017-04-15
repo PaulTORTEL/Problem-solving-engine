c371228b295b93de7f27e05a76bce0e6ae1e3220
Obi-wan77 <mattpil77@hotmail.fr>
2017-04-15 22:27:45 +0200

    Menu dans le main + changements dans les domaines pour répondre au sujet

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "Engine.h"
 #include "XMLParser.h"
 #include <string>
+#include <dirent.h>
 using namespace std;
 
 
@@ -15,45 +16,91 @@ Engine createEngine(const std::string& file) {
 	exit(1);
 }
 
+std::vector<std::string> GetProblems(const char *path) {
+    std::vector<std::string> files;
+    struct dirent *entry;
+    DIR *dp;
+
+    dp = opendir(path);
+    if (dp == NULL) {
+        return files;
+    }
+
+    while ((entry = readdir(dp))) {
+        std::string filename(entry->d_name);
+
+        if (filename.find(".xml") != std::string::npos) {
+            files.push_back(filename);
+        }
+    }
+    closedir(dp);
+    return files;
+}
+
+const std::string* ChooseProblems(const std::vector<std::string>& problems) {
+    std::cout << "Choisir un fichier a charger :" << std::endl;
+    std::cout << "0. Quitter" << std::endl;
+    for (unsigned int i = 0; i < problems.size(); i++) {
+        std::cout << (i + 1) << ". " << problems[i] << std::endl;
+    }
+
+    unsigned int choice = 0;
+    while (choice <= 0 || choice > problems.size()) {
+        std::cout << "Choix : ";
+        std::cin >> choice;
+        if(std::cin.fail()) {
+            std::cin.clear();
+        } else {
+            std::cin.ignore(1, '\n');
+            if(choice == 0)
+                return NULL;
+        }
+    }
+    return &problems[choice-1];
+}
+
+
 int main(int argc, char** argv)
 {
+    const char* folder = "problems/";
+    std::vector<std::string> problems = GetProblems(folder);
 
-	std::string file = "problems/test6.xml";
+    while(true){
 
-    //std::string file = "problems/probleme_4.xml";
+        std::cout << std::endl << "\t    *=====*=====* Programme de resolution de probleme *=====*=====*"<< std::endl;
+        std::cout << "\t*=====*=====* Propagation et Satisfaction de Contraintes  *=====*=====*" << std::endl << std::endl;
+        std::cout << "\tDevs: Vincent COURSAC, Paul TORTEL, Matthieu PILAUDEAU,\n\t      Arthur HEUILLARD & Thomas CORNIER " << std::endl << std::endl;
 
-	//std::string file = "problems/Probleme7.xml";
-	//std::string file = "problems/test6.xml";
-	//std::string file = "problems/Probleme6.xml";
+        const std::string* choice = ChooseProblems(problems);
+        if(choice==NULL)
+            return 0;
+        std::string file = (std::string)folder + "/" + *choice;
 
-	if(argc > 1)
-		file = argv[1];
+        if(argc > 1)
+            file = argv[1];
 
+        Engine engine = createEngine(file);
+        Constraints& constraints = engine.getConstraints();
 
-	Engine engine = createEngine(file);
+        std::cout << std::endl << "\tMethode + contrainte d'abord : " << std::endl<<std::endl;
+        engine.createTree(TraversingOrder::CONSTRAINTS_MOST);
+        std::cout << std::endl;
 
-	Constraints& constraints = engine.getConstraints();
+        std::cout << std::endl << "\tMethode - contrainte d'abord : " << std::endl<<std::endl;
+        engine.createTree(TraversingOrder::CONSTRAINTS_LEAST);
+        std::cout << std::endl;
 
-	/*for(auto it = engine.beginVars(); it != engine.endVars(); it++) {
-		std::cout << *it << std::endl;
-	}*/
+        std::cout << std::endl << "\tMethode classique :" << std::endl<<std::endl;
+        engine.createTree(TraversingOrder::VARIABLES);
+        std::cout << std::endl;
 
+        std::cout << std::endl << "\tMethode domaine le + petit :" << std::endl<<std::endl;
+        engine.createTree(TraversingOrder::DOMAINS);
 
-    std::cout << "\tMethode + contrainte d'abord : " << std::endl;
-    engine.createTree(TraversingOrder::CONSTRAINTS_MOST);
-    std::cout << std::endl;
+        system("PAUSE");
+        system("CLS");
 
-    std::cout << "\tMethode - contrainte d'abord : " << std::endl;
-    engine.createTree(TraversingOrder::CONSTRAINTS_LEAST);
-    std::cout << std::endl;
-
-    std::cout << "\tMethode classique :" << std::endl;
-    engine.createTree(TraversingOrder::VARIABLES);
-    std::cout << std::endl;
-
-    std::cout << "\tMethode domaine le + petit :" << std::endl;
-    engine.createTree(TraversingOrder::DOMAINS);
-
+    }
 
     return 0;
 }
